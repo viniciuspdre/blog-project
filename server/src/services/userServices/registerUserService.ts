95d07e07b56348cf42543cf23f04810329b1a0d9
Pedro Lira <pedrovinilira2005@gmail.com>
2025-04-17 23:55:56 -0300

    (fix): verify if email is already registered

--- server/src/services/userServices/registerUserService.ts
+++ server/src/services/userServices/registerUserService.ts
@@ -1,8 +1,9 @@
 import { hashPassword, comparePassword } from "../../utils/hash-password-helper"
 import { registerUserRepo } from "../../repositories/userRepositories/registerUserRepo";
 import { uploadToS3 } from "../../config/aws-sdk-config";
-import { badRequest, created } from "../../utils/http-helper";
+import { badRequest, conflict, created } from "../../utils/http-helper";
 import { HttpResponse } from "../../models/http-response-models";
+import { PrismaClient } from "../../prisma/generated/prisma";
 
 
 const registerUserService = async (name: string, email: string, password_hash: string, profile_picture_url: Express.Multer.File | undefined): Promise<HttpResponse> => {
@@ -10,7 +11,14 @@ const registerUserService = async (name: string, email: string, password_hash: s
   let response: HttpResponse | null = null;
 
   if (!name || !email || !password_hash) { // verifica se falta algum conteúdo obrigatório
-    response = await badRequest();
+    response = await badRequest("Envie os dados de cadastro corretamente.");
+    return response;
+  }
+
+  const prisma = new PrismaClient();
+
+  if (await prisma.users.findUnique({ where: { email } })) {
+    response = await conflict("Esse e-mail já foi cadastrado.");
     return response;
   }
 
